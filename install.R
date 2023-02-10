a439cf96a2d37d60bb5aeb92033c93c201054afc
Martin Schweinberger <44954005+MartinSchweinberger@users.noreply.github.com>
2023-02-10 12:17:12 +1000

    Update install.R

---
+++ install.R
@@ -0,0 +1,10 @@
+install.packages("boot")
+install.packages("DescTools")
+install.packages("dplyr")
+install.packages("stringr")
+install.packages("ggplot2")
+install.packages("flextable")
+install.packages("ggpubr")
+install.packages("psych")
+install.packages("Rmisc")
+
